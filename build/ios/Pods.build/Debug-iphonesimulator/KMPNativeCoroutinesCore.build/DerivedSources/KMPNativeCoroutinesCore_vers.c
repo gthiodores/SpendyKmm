52ad12bb060b01178fb182322ec0d2e5da781c3a
Gilbert Thiodores <gthiodores71@gmail.com>
2023-05-06 13:48:54 +0800

    git ignore is working now?

--- build/ios/Pods.build/Debug-iphonesimulator/KMPNativeCoroutinesCore.build/DerivedSources/KMPNativeCoroutinesCore_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char KMPNativeCoroutinesCoreVersionString[];
- extern const double KMPNativeCoroutinesCoreVersionNumber;
-
- const unsigned char KMPNativeCoroutinesCoreVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:KMPNativeCoroutinesCore  PROJECT:Pods-1" "\n";
- const double KMPNativeCoroutinesCoreVersionNumber __attribute__ ((used)) = (double)1.;
